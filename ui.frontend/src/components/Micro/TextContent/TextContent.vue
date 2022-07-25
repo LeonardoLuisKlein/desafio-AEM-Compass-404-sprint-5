1208cf0bdfcdf31445d674d8ab06b7e40df6c515
Leonardo Luis Klein <leoonardoluis.klein@outlook.com>
2022-07-25 20:12:33 -0300

    Feat: text component with basic style

---
+++ ui.frontend/src/components/Micro/TextContent/TextContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <p id="idText">{{ text }}</p>
+</template>
+
+<script>
+export default {
+  name: "TextContent",
+  props: {
+    text: {
+      type: String,
+      default: ""
+    },
+    idText: {
+      type: String
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#basicText {
+  font-family: 'Space Mono';
+  font-weight: 400;
+  font-size: 1.5em;
+  color: #4F4F4F;
+}
+</style>
