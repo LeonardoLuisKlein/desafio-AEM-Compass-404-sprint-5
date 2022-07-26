9b7526f65bccea987a447597bb46958d85bf8e50
Leonardo Luis Klein <leoonardoluis.klein@outlook.com>
2022-07-26 10:45:50 -0300

    Feat: image component with props

---
+++ ui.frontend/src/components/Micro/ImageContent/ImageContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <img :src="src" />
+</template>
+
+<script>
+export default {
+  name: 'ImageContent',
+  props: {
+    src: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  max-width: 40vw;
+}
+</style>
