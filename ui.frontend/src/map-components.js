45402fbb98b04d05081671b490e49f4d618c4071
Leonardo Luis Klein <leoonardoluis.klein@outlook.com>
2022-07-26 10:46:29 -0300

    Feat: image component map

--- ui.frontend/src/map-components.js
+++ ui.frontend/src/map-components.js
@@ -8,6 +8,7 @@ import Main from './components/Containers/Main/Main'
 import TextContent from './components/Micro/TextContent/TextContent'
 import Title from './components/Micro/Title/Title'
 import Button from './components/Micro/Button/Button'
+import ImageContent from './components/Micro/ImageContent/ImageContent'
 import MultifieldExampleComponent from './components/MultifieldExampleComponent/MultifieldExampleComponent'
 import {
   withComponentMappingContext,
@@ -36,6 +37,9 @@ MapTo('vue/components/multifield-example-component')(
   MultifieldExampleComponent,
   EditConfig
 )
+
+// ImageContent Component Mapping
+MapTo('vue/components/image-content')(ImageContent, EditConfig)
 
 // Button Component Mapping
 MapTo('vue/components/button')(Button, EditConfig)
