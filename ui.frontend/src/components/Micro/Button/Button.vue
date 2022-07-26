41d1ce7544307bb1cf233ea464e26bab99b9cab2
Leonardo Luis Klein <leoonardoluis.klein@outlook.com>
2022-07-25 21:20:12 -0300

    Feat: adding button component with basic style

---
+++ ui.frontend/src/components/Micro/Button/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button :id="idButton">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    idButton: {
+      type: String
+    },
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+#basicButton {
+  width: 13.5em;
+  height: 4.25em;
+  color: #333333;
+  font-family: 'Space Mono';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 0.875em;
+  color: #FFFFFF
+}
+</style>
